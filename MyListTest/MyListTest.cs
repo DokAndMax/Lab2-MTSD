b518770a502cb8300bd7cdeaccabf4ee872a8d5d
Maks <maxim.kryvenok@gmail.com>
2023-03-21 04:26:21 +0200

    Make unit tests far all other methods
    There are unit tests for methods:
    - Delete;
    - DeleteAll;
    - Get;
    - Clone;
    - Reverse;
    - FindFirst;
    - FindLast
    - Clear;
    - Extend.

--- MyListTest/MyListTest.cs
+++ MyListTest/MyListTest.cs
@@ -46,48 +46,136 @@ namespace MyListTest
         }
 
         [TestMethod]
+        public void Test_Delete_Element_At_OutOfRange_Index()
+        {
+            MyList myList = new();
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => myList.Delete(3));
+        }
+
+        [TestMethod]
+        public void Test_Delete_Returned_Element()
+        {
+            MyList myList = new();
+
+            myList.Append('g');
+            
+            Assert.AreEqual('g', myList.Delete(0));
+        }
+
+        [TestMethod]
         public void Test_Delete_Element_At_Index()
         {
+            MyList myList = new();
+
+            myList.Append('g');
+            myList.Delete(0);
+
+            Assert.AreEqual(0, myList.Length());
         }
 
         [TestMethod]
         public void Test_Delete_All_Elements()
         {
+            MyList myList = new();
+
+            myList.Append('h');
+            myList.Append('h');
+            myList.DeleteAll('h');
+
+            Assert.AreEqual(0, myList.Length());
         }
 
         [TestMethod]
         public void Test_Get_Element_At_Index()
         {
+            MyList myList = new();
+
+            myList.Append('i');
+
+            Assert.AreEqual(0, myList.Get(0));
+        }
+
+        [TestMethod]
+        public void Test_Get_Element_At_OutOfRange_Index()
+        {
+            MyList myList = new();
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => myList.Get(3));
         }
 
         [TestMethod]
         public void Test_Clone_List()
         {
+            MyList myList = new();
+
+            myList.Append('j');
+            MyList clone = myList.Clone();
+
+            Assert.IsNotNull(clone);
+            Assert.AreEqual('j', clone.Get(0));
         }
 
         [TestMethod]
         public void Test_Reverse_List()
         {
+            MyList myList = new();
+
+            myList.Append('k');
+            myList.Append('l');
+            myList.Reverse();
+
+            Assert.AreEqual('l', myList.Get(0));
+            Assert.AreEqual('k', myList.Get(1));
         }
 
         [TestMethod]
         public void Test_Find_First_Element()
         {
+            MyList myList = new();
+
+            myList.Append('l');
+            myList.Append('m');
+            myList.Append('m');
+
+            Assert.AreEqual(1, myList.FindFirst('m'));
         }
 
         [TestMethod]
         public void Test_Find_Last_Element()
         {
+            MyList myList = new();
+
+            myList.Append('n');
+            myList.Append('n');
+
+            Assert.AreEqual(1, myList.FindLast('m'));
         }
 
         [TestMethod]
         public void Test_Clear_List()
         {
+            MyList myList = new();
+
+            myList.Append('o');
+            myList.Append('p');
+            myList.Clear();
+
+            Assert.AreEqual(0, myList.Length());
         }
 
         [TestMethod]
         public void Test_Extend_With_List()
         {
+            MyList myList = new();
+            myList.Append('q');
+            MyList myList2 = new();
+            myList2.Append('r');
+
+            myList.Extend(myList2);
+
+            Assert.AreEqual(2, myList.Length());
+            Assert.AreEqual('r', myList.Get(1));
         }
     }
 }
