76cfa619413a3a76f5073dad48dcf8f36d888fde
Maks <maxim.kryvenok@gmail.com>
2023-03-21 02:51:48 +0200

    Create the structure of the unit tests for list

---
+++ MyListTest/MyListTest.cs
@@ -0,0 +1,68 @@
+using Lab2_MTSD;
+
+namespace MyListTest
+{
+    [TestClass]
+    public class MyListTest
+    {
+        [TestMethod]
+        public void Test_Get_Length()
+        {
+        }
+
+        [TestMethod]
+        public void Test_Append_Element()
+        {
+        }
+
+        [TestMethod]
+        public void Test_Insert_Element_At_Index()
+        {
+        }
+
+        [TestMethod]
+        public void Test_Delete_Element_At_Index()
+        {
+        }
+
+        [TestMethod]
+        public void Test_Delete_All_Elements()
+        {
+        }
+
+        [TestMethod]
+        public void Test_Get_Element_At_Index()
+        {
+        }
+
+        [TestMethod]
+        public void Test_Clone_List()
+        {
+        }
+
+        [TestMethod]
+        public void Test_Reverse_List()
+        {
+        }
+
+        [TestMethod]
+        public void Test_Find_First_Element()
+        {
+        }
+
+        [TestMethod]
+        public void Test_Find_Last_Element()
+        {
+        }
+
+        [TestMethod]
+        public void Test_Clear_List()
+        {
+        }
+
+        [TestMethod]
+        public void Test_Extend_With_List()
+        {
+        }
+    }
+}
