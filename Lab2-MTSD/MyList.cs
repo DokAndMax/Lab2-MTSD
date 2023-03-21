ff1a8bf9ce96d58a3e883939dd8dbb86f0d22625
Maks <maxim.kryvenok@gmail.com>
2023-03-21 02:30:13 +0200

    Create the initial structure of the custom list

---
+++ Lab2-MTSD/MyList.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_MTSD
+{
+    internal class MyList
+    {
+        public int Length()
+        {
+            return 0;
+        }
+
+        public void Append(char element)
+        {
+        }
+
+        public void Insert(char element, int index)
+        {
+        }
+
+        public char Delete(int index)
+        {
+            return '\0';
+        }
+
+        public void DeleteAll(char element)
+        {
+        }
+
+        public char Get(int index)
+        {
+            return '\0';
+        }
+
+        public MyList Clone()
+        {
+            return this;
+        }
+
+        public void Reverse()
+        {
+        }
+
+        public int FindFirst(char element)
+        {
+            return '\0';
+        }
+
+        public int FindLast(char element)
+        {
+            return '\0';
+        }
+
+        public void Clear()
+        {
+        }
+
+        public void Extend(MyList elements)
+        {
+        }
+    }
+}
